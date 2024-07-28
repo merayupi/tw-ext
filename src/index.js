fd8c51112c4b7140be07d30e877c80b2132b47c5
meracon <bintang555pamungkas@gmail.com>
2024-07-28 20:52:26 +0700

    chore: Refactor code for improved performance and layout

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const attachButton = async (username) => {
     const element2 = document.querySelector("#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div.css-175oi2r.r-kemksi.r-1kqtdi0.r-1ua6aaf.r-th6na.r-1phboty.r-16y2uox.r-184en5c.r-1c4cdxw.r-1t251xo.r-f8sm7e.r-13qz1uu.r-1ye8kvj > div > div:nth-child(3) > div > div > div > div.css-175oi2r.r-3pj75a.r-ttdzmv.r-1ifxtd0 > div.css-175oi2r.r-13awgt0.r-18u37iz.r-1w6e6rj > div:nth-child(2)");
 
     if (element2) {
-        const existingButton = element.parentNode.querySelector('.history-btn');
+        const existingButton = element.parentNode.querySelector('.recentfollowing-btn');
         if (existingButton) {
             existingButton.remove();
         }
@@ -12,32 +12,15 @@ const attachButton = async (username) => {
             existingButton2.remove();
         }
 
-        const button = await createButtonHistory(username);
         const button2 = await createButtonFirstFollower(username);
-        
-        button.className = "history-btn";
+        const button3 = await createButtonRecentFollowing(username);
+
         button2.className = "firstfollower-btn";
-        element.parentNode.appendChild(button);
+        button3.className = "recentfollowing-btn";
+
         element2.parentNode.appendChild(button2);
-    
+        button2.parentNode.appendChild(button3);
     }
-}
-
-const createButtonHistory = async (username) => {
-    const button = document.createElement("button");
-    button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="height: 20px; width: auto; vertical-align: middle;"><path fill="#B100FF" d="M12,2A10,10,0,0,0,5.12,4.77V3a1,1,0,0,0-2,0V7.5a1,1,0,0,0,1,1H8.62a1,1,0,0,0,0-2H6.22A8,8,0,1,1,4,12a1,1,0,0,0-2,0A10,10,0,1,0,12,2Zm0,6a1,1,0,0,0-1,1v3a1,1,0,0,0,1,1h2a1,1,0,0,0,0-2H13V9A1,1,0,0,0,12,8Z"></path></svg>';
-    button.style.background = 'transparent';
-    button.style.padding = '0px';
-    button.style.cursor = "pointer";
-    button.style.border = "0px";
-    button.style.margin = "0em 0em 0em 0.5em";
-
-    button.onclick = async function(event) {
-        const result  = createModalUI();
-        await createUIResult(username, result);
-    };
-    
-    return button;
 }
 
 const createButtonFirstFollower = async (username) => {
@@ -63,6 +46,31 @@ const createButtonFirstFollower = async (username) => {
 
     return button;
 }
+
+const createButtonRecentFollowing = async (username) => {
+    const button = document.createElement("div");
+    button.innerText = 'Recent Following';
+    button.style.background = 'transparent';
+    button.style.marginBottom = '10px';
+    button.style.marginLeft = '30px';
+    button.style.cursor = "pointer";
+    button.style.color = "#B100FF";
+    button.style.fontWeight = "bold";
+    button.style.fontSize = "16px";
+    button.style.fontFamily = "TwitterChirp, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif";
+    button.style.display = "flex";
+    button.style.alignItems = "stretch";
+    button.style.flexDirection = "column";
+    button.style.position = "relative";
+
+    button.onclick = async function(event) {
+        const result  = createModalUI();
+        await createUIRecentFollowingResult(username, result);
+    };
+
+    return button;
+}
+
 const createModalUI = () => {
     document.body.style.overflow = 'hidden';
 
@@ -173,26 +181,55 @@ const getAllFollowers = async (username, maxConcurrent = 5) => {
     }
   
     return allFollowers;
-  };
-const createUIResult = async (username, result) => {
-    let outputHTML = '';
-    const usernames = await getAllHistoryUsername(username);
+};
 
-    usernames.forEach(user => {
-        outputHTML += ` <div class="container-content">
-                            <img src="pfp.png" alt="pfp">
-                            <div class="content">
-                                <h2 class="username">Username: ${user.username}</h2>
-                                <p class="last-checked">Date:</p>
-                            </div>
-                        </div>`;
-    });
-
-    const outputContainer = document.createElement("div");
-    outputContainer.classList.add("container-main");
-    outputContainer.innerHTML = outputHTML;
-
-    result.appendChild(outputContainer);
+const getAllFollowing = async(username,  maxConcurrent = 5) => {
+    const fetchFollowers = async (cursor) => {
+        const url = new URL('https://api.twitter.com/1.1/friends/list.json');
+        url.search = new URLSearchParams({
+          cursor,
+          screen_name: username,
+          count: 200,
+          skip_status: true,
+          include_user_entities: false,
+        }).toString();
+    
+        const res = await fetch(url, {
+          headers: {
+            'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
+          }
+        });
+    
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+    
+        return res.json();
+      };
+    
+      let allFollowings = [];
+      let cursors = [-1];
+      
+      while (cursors.length > 0) {
+        const batch = cursors.splice(0, maxConcurrent);
+        const responses = await Promise.all(batch.map(fetchFollowers));
+        
+        for (const { users, next_cursor } of responses) {
+          allFollowings.push(...users.map(user => ({
+            id: user.id_str,
+            name: user.name,
+            username: user.screen_name,
+            bio: user.description,
+            follower: user.followers_count,
+            following: user.friends_count,
+            pfp: user.profile_image_url_https,
+            post: user.statuses_count,
+            age: `<t:${Math.floor(new Date(user.created_at).getTime() / 1000)}:R>`
+          })));
+          
+          if (next_cursor) cursors.push(next_cursor);
+        }
+      }
+    
+      return allFollowings;
 }
 
 const createUIFirstFollowerResult = async (username, result) => {
@@ -207,6 +244,34 @@ const createUIFirstFollowerResult = async (username, result) => {
     let outputHTML = '';
 
     firstfollowers.forEach(user => {
+        outputHTML += ` <div class="container-content">
+                            <img src="${user.pfp}" alt="pfp">
+                            <div class="content">
+                                <h2 class="username"><a href="https://twitter.com/${user.username}" target="_blank">${user.username}</a> |  Follower: ${user.follower}</h2>
+                                <p class="bio">${user.bio}</p>
+                            </div>
+                        </div>`;
+    });
+
+    const outputContainer = document.createElement("div");
+    outputContainer.classList.add("container-main");
+    outputContainer.innerHTML = outputHTML;
+
+    result.appendChild(outputContainer);
+};
+
+const createUIRecentFollowingResult = async (username, result) => {
+    const loading = document.createElement("div");
+    loading.classList.add("loader");
+    result.appendChild(loading);
+
+    const followings = await getAllFollowing(username);
+    const recentfollowing = followings.slice(0,80);
+
+    loading.remove();
+    let outputHTML = '';
+
+    recentfollowing.forEach(user => {
         outputHTML += ` <div class="container-content">
                             <img src="${user.pfp}" alt="pfp">
                             <div class="content">
